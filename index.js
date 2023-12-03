500ddda0e8d6ffbbad59e537933a80dc23616f3a
John Clampet <jsclampet@gmail.com>
2023-12-03 15:52:28 -0600

    Added some more methods, no validation yet

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 const genres = [
   { name: "horror", id: 1 },
   { name: "comedy", id: 2 },
@@ -17,6 +19,22 @@ app.get("/api/genres/:id", (req, res) => {
   const genre = genres.find((g) => g.id === parseInt(req.params.id));
   if (!genre) return res.status(404).send("genre not found");
 
+  res.send(genre);
+});
+
+app.post("/api/genres", (req, res) => {
+  const genre = {
+    name: req.body.name,
+    id: genres.length + 1,
+  };
+
+  genres.push(genre);
+  res.send(genre);
+});
+
+app.put("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  genre.name = req.body.name;
   res.send(genre);
 });
 
