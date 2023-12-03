4d93aa7ad33ef9add9405a4d284ef3129f6740de
John Clampet <jsclampet@gmail.com>
2023-12-03 15:05:22 -0600

    initial commit

---
+++ SCRATCHPAD.md
@@ -0,0 +1,26 @@
+# Vidly
+*Movie genre API with CRUD capabilities*
+- *Create genre*
+- *Read genre*
+- *Update genre*
+- *Delete genre*
+
+### CRUD HTTP Methods 
+- Create = POST
+- Read = GET
+- Update = PUT
+- Delete = DELETE
+
+### Third-party Libraries
+- Express
+- - Handles HTTP
+- Joi
+- - Handles validation 
+- #### Global Library
+- Nodemon
+- - More efficient monitoring for app
+- - Similar to live server, updates on vs code save
+
+#### Helpful Resources
+[Joi docs](https://joi.dev/)
+[Express docs](https://expressjs.com/en/4x/api.html#req)
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const genres = [
+  { name: "horror", id: 1 },
+  { name: "comedy", id: 2 },
+  { name: "romance", id: 3 },
+  { name: "drama", id: 4 },
+];
+
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
+});
+
+app.get("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send("genre not found");
+
+  res.send(genre);
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
